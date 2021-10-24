8495e34534ae1967e870bb76a5ed2e65c0a773e1
MarcPalacio <mpalacio2020@gmail.com>
2021-10-24 19:24:30 -0400

    Fourth commit with test cases pseudocode

---
+++ src/test/java/baseline/ApplicationTest.java
@@ -0,0 +1,92 @@
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 1 Solution
+ *  Copyright 2021 Marc Palacio
+ */
+
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+    @Test
+    void testAddingElements1(){
+        // Create an arraylist
+        // Add an element or 2 to the arraylist
+        // Compare the actual and expected arraylist (Hopefully it shows true)
+    }
+
+    @Test
+    void testAddingElements2(){
+        // Create an arraylist
+        // Add an element or 2 to the arraylist
+        // Compare the actual and expected arraylist (Hopefully it shows true)
+    }
+
+    @Test
+    void testRemovingElements1(){
+        // Create an arraylist
+        // Delete an element or 2 from the arraylist
+        // Compare the actual and expected arraylist (Hopefully it shows true)
+    }
+
+    @Test
+    void testRemovingElements2(){
+        // Create an arraylist
+        // Delete an element or 2 from the arraylist
+        // Compare the actual and expected arraylist (Hopefully it shows true)
+    }
+
+    @Test
+    void testEditingElements1(){
+        // Create an arraylist
+        // Edit an element from the arraylist (Event title)
+        // Compare the actual and expected arraylist (Hopefully it shows true)
+    }
+
+    @Test
+    void testEditingElements2(){
+        // Create an arraylist
+        // Edit an element from the arraylist (Description)
+        // Compare the actual and expected arraylist (Hopefully it shows true)
+    }
+
+    @Test
+    void testEditingElements3(){
+        // Create an arraylist
+        // Edit an element from the arraylist (Due Date)
+        // Compare the actual and expected arraylist (Hopefully it shows true)
+    }
+
+    @Test
+    void testEditingElements4(){
+        // Create an arraylist
+        // Edit an element from the arraylist (Is completed or not)
+        // Compare the actual and expected arraylist (Hopefully it shows true)
+    }
+
+    @Test
+    void testCreatingOutput1(){
+        // Creates a string from arraylist
+        // Compares the actual and expected string
+    }
+
+    @Test
+    void testCreatingOutput2(){
+        // Creates a string from arraylist
+        // Compares the actual and expected string
+    }
+
+    @Test
+    void testCreatingInput1(){
+        // Creates input list from file input
+        // Compares the actual and expected arraylist
+    }
+
+    @Test
+    void testCreatingInput2(){
+        // Creates input list from file input
+        // Compares the actual and expected arraylist
+    }
+}
